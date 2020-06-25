9e4e6bfd09b871d7574b9ec378dd8c24cc53b87f
TkhiienLok <tkhiien@gmail.com>
2020-06-25 15:01:15 +0300

    Added loader component

--- src/components/random-recipe/random-recipe.js
+++ src/components/random-recipe/random-recipe.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HealthyFoodService from '../../services/healthy-food-service';
+import Spinner from '../spinner';
 import './random-recipe.css';
 
 
@@ -9,7 +10,8 @@ export default class RandomRecipe extends Component {
   state = {
     recipe: {},
     quantity: 100,
-    unit: 'g'
+    unit: 'g',
+    loading: true
   }
 
   constructor() {
@@ -18,7 +20,10 @@ export default class RandomRecipe extends Component {
   }
 
   onRecipeLoaded = (recipe) => {
-    this.setState({ recipe });
+    this.setState({ 
+      recipe,
+      loading: false
+    });
   }
 
   updateRecipe() {
@@ -29,53 +34,64 @@ export default class RandomRecipe extends Component {
   }
 
   render()  {
-    const { recipe: { title, calories, totalWeight, nutrients={}, imageURL, diets=[], cautions=[], url }, quantity, unit, } = this.state;
-
+    const { recipe, quantity, unit, loading } = this.state;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <RecipeView recipe={recipe} quantity={quantity} unit={unit}/>: null;
     return (
       <div className="random-recipe jumbotron rounded">
-        <img src={imageURL} className="recipe-image" alt="dish look"/>
-        <div>
-          <h4>{ title }</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Calories -</span>
-              <span>{ Math.round(calories * quantity / totalWeight) } / { quantity } { unit }</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diet -</span>
-                {
-                  diets.map((diet, idx, arr) => {
-                    return(<span key={diet}>{ diet } { idx < arr.length - 1 ? ',' : '' } </span>);
-                  })
-                }
-            </li>
-            { cautions.length 
-            ? 
-              (<li className="list-group-item">
-                <span className="term caution">!</span>
-                  {
-                    cautions.map((caution, idx, arr) => {
-                      return(<span key={caution}>{ caution } { idx < arr.length - 1 ? ',' : '' } </span>);
-                    })
-                  }
-              </li>)
-            :
-              null }
-            <li className="list-group-item">Nutrients:
-              <ul>
-                {
-                  Object.keys(nutrients).map((nutrient) => {
-                    return(<li key={nutrient}>{ nutrient }: { nutrients[nutrient] }</li>);
-                  })
-                }
-              </ul>
-            </li>
-            <li className="list-group-item">
-              <a target="_blank" rel="noopener noreferrer" href={url}>Go to website</a>
-            </li>
-          </ul>
-        </div>
+        {spinner}
+        {content}
       </div>
     );
   }
+};
+
+const RecipeView = ({recipe, quantity, unit}) => {
+  const { title, calories, totalWeight, nutrients={}, imageURL, diets=[], cautions=[], url } = recipe;
+  return (
+    <React.Fragment>
+      <img src={imageURL} className="recipe-image" alt="dish look"/>
+      <div>
+        <h4>{ title }</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Calories -</span>
+            <span>{ Math.round(calories * quantity / totalWeight) } / { quantity } { unit }</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diet -</span>
+              {
+                diets.map((diet, idx, arr) => {
+                  return(<span key={diet}>{ diet } { idx < arr.length - 1 ? ',' : '' } </span>);
+                })
+              }
+          </li>
+          { cautions.length 
+          ? 
+            (<li className="list-group-item">
+              <span className="term caution">!</span>
+                {
+                  cautions.map((caution, idx, arr) => {
+                    return(<span key={caution}>{ caution } { idx < arr.length - 1 ? ',' : '' } </span>);
+                  })
+                }
+            </li>)
+          :
+            null }
+          <li className="list-group-item">Nutrients:
+            <ul>
+              {
+                Object.keys(nutrients).map((nutrient) => {
+                  return(<li key={nutrient}>{ nutrient }: { nutrients[nutrient] }</li>);
+                })
+              }
+            </ul>
+          </li>
+          <li className="list-group-item">
+            <a target="_blank" rel="noopener noreferrer" href={url}>Go to website</a>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
 };
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return(
+    <div className="spinner-wrapper">
+      <div className="loadingio-spinner-spinner-o4wkynaniyd">
+        <div className="ldio-w4umsp34b6">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
